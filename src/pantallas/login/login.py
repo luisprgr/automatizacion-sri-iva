72005bcc2200ade430bf99b3a682126e7c8b76bd
luisprgr <50538689+luisprgr@users.noreply.github.com>
2024-04-24 02:11:55 -0500

    :bug: fixes login constants names

--- src/pantallas/login/login.py
+++ src/pantallas/login/login.py
@@ -4,23 +4,23 @@ from selenium.webdriver.common.by import By
 
 from src.utilidades.utilidades import guardar_captura
 from src.pantallas.login.ids import (
-    INPUT_USER_ID,
-    INPUT_PASSWORD_ID,
-    BUTTON_LOGIN_ID
+    USER_INPUT_ID,
+    PASSWORD_INPUT_ID,
+    LOGIN_BUTTON_ID
 )
 
 
 def login_sri(driver, guardar_capturas: bool, ruc: str, password: str):
     wait = WebDriverWait(driver, 10)
 
-    wait.until(expected.presence_of_element_located((By.ID, INPUT_USER_ID)))
+    wait.until(expected.presence_of_element_located((By.ID, USER_INPUT_ID)))
 
     if guardar_capturas:
         guardar_captura(driver, 'login')
 
-    ruc_input = driver.find_element(by=By.ID, value=INPUT_USER_ID)
-    password_input = driver.find_element(by=By.ID, value=INPUT_PASSWORD_ID)
-    login_button = driver.find_element(by=By.ID, value=BUTTON_LOGIN_ID)
+    ruc_input = driver.find_element(by=By.ID, value=USER_INPUT_ID)
+    password_input = driver.find_element(by=By.ID, value=PASSWORD_INPUT_ID)
+    login_button = driver.find_element(by=By.ID, value=LOGIN_BUTTON_ID)
 
     ruc_input.send_keys(ruc)
     password_input.send_keys(password)
