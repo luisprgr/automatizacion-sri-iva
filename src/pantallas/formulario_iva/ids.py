ea3c6b7e3d9093a325d26db18532b53d68ceb12d
luisprgr <50538689+luisprgr@users.noreply.github.com>
2024-04-24 01:40:00 -0500

    :recycle: replaces ids in seleccionar_periodo_fiscal function and preguntas function with constants

---
+++ src/pantallas/formulario_iva/ids.py
@@ -0,0 +1,14 @@
+OBLIGACION_DROPDOWN_ID = "frmFlujoDeclaracion:somObligacion_label"
+OBLIGACION_DECLARACION_IVA_2011_ELEMENT_ID = "frmFlujoDeclaracion:somObligacion_1"
+DIALOGO_DESCARTABLE_BUTTON_XPATH = ( 
+    "// div[@id='frmFlujoDeclaracion:dialogoMensajesPersonalizados']"
+    "//button[contains(@id, 'frmFlujoDeclaracion')]"
+)
+DATE_SELECTOR_ID = "frmFlujoDeclaracion:calPeriodo"
+DATE_ELEMENT_CSS_SELECTOR = "a.button-{month}"
+PERIODO_FISCAL_SIGUIENTE_BUTTON_ID = "frmFlujoDeclaracion:btnObligacionSiguiente"
+DESCARTAR_BORRADOR_BUTTON_XPATH = "//button/span[contains(text(), 'Rechazar')]"
+
+PREGUNTAS_SIGUIENTE_BUTTON_ID = "frmFlujoDeclaracion:btnPerfiladorSiguiente"
+
+
--- src/pantallas/formulario_iva/preguntas.py
+++ src/pantallas/formulario_iva/preguntas.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 from src.utilidades.utilidades import guardar_captura
 
+from src.pantallas.formulario_iva.ids import PREGUNTAS_SIGUIENTE_BUTTON_ID
 
 def preguntas(
     driver,
@@ -13,11 +14,14 @@ def preguntas(
     wait = WebDriverWait(driver, 10)
 
     wait.until(expected.presence_of_element_located(
-        (By.ID, 'frmFlujoDeclaracion:btnPerfiladorSiguiente')))
+        (By.ID, PREGUNTAS_SIGUIENTE_BUTTON_ID)))
 
     if guardar_capturas:
         guardar_captura(driver, 'preguntas')
 
+    import time
+    time.sleep(5)
+
     next_step_button = driver.find_element(
-        by=By.ID, value='frmFlujoDeclaracion:btnPerfiladorSiguiente')
+        by=By.ID, value=PREGUNTAS_SIGUIENTE_BUTTON_ID)
     next_step_button.click()
